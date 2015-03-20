2af1b568454f25cdbe9affe3795ede481924acb0
PeoceWang-vanke <wangxm-wr@vanke.com>
2015-03-20 16:58:46 +0800

    add syn method  download catalog picture

--- src/getPic/__init__.py
+++ src/getPic/__init__.py
@@ -1,72 +1,90 @@
-# coding=utf-8
-import re, os, urllib, sys, socket
+# -*- coding=utf-8 -*-
+import re, os, urllib, sys, socket, string
 from itertools import count
 
-def utf2defcode(mstring):  # 转换为本地编码，解决中文乱码问题
-    defcode = sys.getfilesystemencoding()
-    return mstring.decode('utf-8').encode(defcode)
 def getHtml(url):  # 获取网页源代码
     page = urllib.urlopen(url)
     html = page.read()
     return html
-def getTitle(html):  # 获取专辑名
+
+def rUnsupportChar(s):  # 替换不能作为目录名的字符 <> : * " ? |
+    unSupChar = r'''
+    <>:*"?\
+    '''
+    supChar=r'''
+    ()-^~$-
+    '''
+    trans=string.maketrans(unSupChar,supChar)
+    s = s.translate(trans, "")
+    return s
+
+def downLoadImg(imglist,title):  # 下载图片到本地
+    x=1
+    for imgurl in imglist:
+        picPath=os.getcwd() + '\\' + title + '\%s.jpg' % x
+        print "download: "+picPath
+        urllib.urlretrieve(imgurl, unicode(picPath))
+        x += 1
+
+
+def createNewFolder(html):  # 创建文件夹，返回文件夹名
     reg = r'<h2>(.+)</h2>'
     titleRe = re.compile(reg)
     title = re.findall(titleRe, html)
+    title[0]=rUnsupportChar(title[0])
+    dirPath = os.getcwdu() + unicode("\\") + unicode(title[0])
+    if(os.path.exists(dirPath) == False):
+        os.makedirs(unicode(title[0]))
     return title[0]
-def getImg(html, title):  # 下载图片到本地
-    reg = r'src=\"(.+\.jpg)'
-    imgre = re.compile(reg)
-    imglist = re.findall(imgre, html)
-    x = 1
-    for imgurl in imglist:
-        urllib.urlretrieve(imgurl, os.getcwd() + '\\' + title + '\%s.jpg' % x)
-        x += 1
-    return imglist      
-def createNewFolder(folderName):
-    os.makedirs(folderName)
     
-def getSecondPageList(html):  # 获取专辑里二级页面列表  
-    reg = r'<a href="([^\s]*?item[^\s]*?)">'
+def getItemsUrlList(url):  # 获取图片详情URL列表 
+    html = getHtml(url)
+    reg = r'<a href="([^\s]*?item/[\d]*?)">'
     mre = re.compile(reg)
     pagelist = re.findall(mre, html)
-    return pagelist
-def getPic(html):  # 获取二级页面中图片URL
+    return list(set(pagelist))
+
+def getPicUrl(url):  # 获取图片URL
+    html = getHtml(url)
     reg = r'href="([^\s]*?jpg)"'
     mre = re.compile(reg)
-    piclist = re.findall(mre, html)
-    return piclist
-def getAllFirstPageURL(html):
+    picUrl = re.findall(mre, html)
+    print picUrl
+    return picUrl
+
+def getAllPageUrl(html):  # 获取专辑下所以分页URL列表
     reg = r'<a href="([^s]+?p=\d)">'
     mre = re.compile(reg)
     pagelist = re.findall(mre, html)
-    return pagelist
-    
-socket.setdefaulttimeout(60)  # 防止网络延迟导致崩溃
- 
-albumURL = "http://www.topit.me/album/1353605"
-
-html = getHtml(albumURL)
- 
-firstPagelist = [albumURL] + getAllFirstPageURL(html)
- 
-title = getTitle(html)
+    return [albumURL, ] + pagelist
   
-createNewFolder(utf2defcode(title))
 
-firstPagelistLen = len(firstPagelist)
+#防止网络延迟导致崩溃   
+socket.setdefaulttimeout(60)        
+  
+albumURL = "http://www.topit.me/album/1042226"
+  
+#获取html页面源代码
+html = getHtml(albumURL)
+  
+#创建专辑名为名的文件夹
+title=createNewFolder(html)
 
-x = 1;
+#获取该专辑的所以分页URL
+pageList=getAllPageUrl(html)
+  
+#遍历图片详情页URL
+imgDetialList=[]
+for pageitem in pageList:
+    imgDetialList+=getItemsUrlList(pageitem)
+print imgDetialList
 
-for k in range(0, firstPagelistLen):
-    secondPagelist = getSecondPageList(getHtml(firstPagelist[k]))[::2]
-    secondPagelistLen = len(secondPagelist)
-    for i in range(0, secondPagelistLen):
-        pichtml = getHtml(secondPagelist[i])
-        picURL = ''.join(getPic(pichtml))
-        mpwd = os.getcwd() + '\\' + title + '\\'
-        mpwd = unicode(mpwd, 'utf8')
-        print mpwd
-        print "Download %s.jpg" % x
-        urllib.urlretrieve(picURL, mpwd + '\%s.jpg' % x)
-        x = x + 1
+#获得图片URL
+imgList=[]
+for imgitem in imgDetialList:
+    imgList+=getPicUrl(imgitem)
+    
+#下载图片
+downLoadImg(imgList, title)
+
+
